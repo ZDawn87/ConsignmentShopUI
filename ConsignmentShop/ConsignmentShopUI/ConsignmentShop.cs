8f68718bf1c19dbd70470aad5fae1c8726e250c0
ZDawn87 <34373051+ZDawn87@users.noreply.github.com>
2020-03-07 23:43:31 -0700

    Add files via upload

---
+++ ConsignmentShop/ConsignmentShopUI/ConsignmentShop.cs
@@ -0,0 +1,52 @@
+﻿using ConsignmentShopLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsignmentShopUI
+{
+    public partial class ConsignmentShop : Form
+    {
+        private Store store = new Store();
+        public ConsignmentShop()
+        {
+            InitializeComponent();
+            SetupData();
+        }
+
+        private void SetupData()
+        {
+            Vendor demoVendor = new Vendor();
+
+            /*  demoVendor.FirstName = "Bill";
+              demoVendor.LastName = "Smith";
+              demoVendor.Commission = .5;
+
+              store.Vendors.Add(demoVendor);
+
+              demoVendor = new Vendor();
+
+              demoVendor.FirstName = "Sue";
+              demoVendor.LastName = "Jones";
+              demoVendor.Commission = .5;
+
+              store.Vendors.Add(demoVendor);
+              */
+            store.Vendors.Add(new Vendor { FirstName = "Bill", LastName = "Smith" });
+            store.Vendors.Add(new Vendor { FirstName = "Sue", LastName = "Jones" });
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
